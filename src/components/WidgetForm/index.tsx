f1bb4d517f2d88ec00493659d9c5bf5f514cfb31
LuccaDi <lucca_di@hotmail.com>
2022-05-10 00:08:39 -0300

    Adding background image

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -64,7 +64,7 @@ export function WidgetForm() {
       )}
 
       <footer className="text-xs text-neutral-400">
-        Developed with ♥ by{' '}
+        Developed with ❤️ by{' '}
         <a
           className="underline underline-offset-2"
           href="https://github.com/LuccaDi"
